a5ffea15b696f0c0d2df1d694d20bfab1418cc3d
Oleksandr Fediashov <olfedias@microsoft.com>
2020-10-15 10:22:38 +0200

    add more commands to simplify debug

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,44 @@
 const BundleAnalyzerPlugin = require("webpack-bundle-analyzer")
   .BundleAnalyzerPlugin;
+const TerserPlugin = require("terser-webpack-plugin");
 
-module.exports = {
-  mode: "production",
+const webpackConfig = {
+  mode: process.env.WEBPACK_ENV || "development",
   entry: {
     main: "./main.js",
   },
-  plugins: [new BundleAnalyzerPlugin()],
+  plugins: [
+    new BundleAnalyzerPlugin({ analyzerMode: "static", openAnalyzer: false }),
+  ],
+
+  ...(process.env.WEBPACK_DEBUG && {
+    optimization: {
+      minimizer: [
+        new TerserPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: true,
+          terserOptions: {
+            mangle: false,
+            output: {
+              beautify: true,
+              comments: true,
+              preserve_annotations: true,
+            },
+          },
+        }),
+      ],
+    },
+    output: {
+      pathinfo: true
+    },
+    stats: {
+      optimizationBailout: true,
+    },
+  }),
 };
+
+module.exports = webpackConfig;
+
+console.log(`Webpack mode: ${webpackConfig.mode}`);
+console.log(`Webpack debug: ${process.env.WEBPACK_DEBUG || "false"}`);
